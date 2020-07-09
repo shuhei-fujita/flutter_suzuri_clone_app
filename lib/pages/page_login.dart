b7557c9c14acb08fdc28ab02c826e94d87ea3d4b
syuheifujita <syuheifujita0117@gmail.com>
2020-07-10 05:12:45 +0900

    Execution failed for task ':app:mergeDexDebug'. エラーの解消

--- lib/pages/page_login.dart
+++ lib/pages/page_login.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_hands_on/pages/page_profile.dart';
 
 class PageLogin extends StatelessWidget {
+
+  final _auth = FirebaseAuth.instance;
+  String email;
+  String password;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,13 +17,46 @@ class PageLogin extends StatelessWidget {
       body: Center(
         child: Column(
           children: <Widget>[
-            TextField(),
-            TextField(),
+            TextField(
+              keyboardType: TextInputType.emailAddress,
+              onChanged: (value) {
+                email = value;
+              },
+              decoration: InputDecoration(
+                labelText: "Email",
+              ),
+            ),
+            TextField(
+              keyboardType: TextInputType.visiblePassword,
+              obscureText: true,
+              onChanged: (value) {
+                password = value;
+              },
+              decoration: InputDecoration(
+                labelText: "Password",
+              ),
+            ),
             RaisedButton(
                 child: Text("ログイン"),
                 color: Colors.blue,
                 textColor: Colors.white,
-                onPressed: (){
+                onPressed: () async {
+                  print("email: " + email);
+                  print("password: " + password);
+
+                  try {
+                    final newUser = await _auth.createUserWithEmailAndPassword(
+                        email: email, password: password);
+
+                    if(newUser != null) {
+                      Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => PageProfile()),
+                      );
+                    }
+                  } catch (e) {
+                    print(e);
+                  }
+
                   Navigator.push(context,
                     MaterialPageRoute(builder: (context) => PageLogin()),
                   );
--- lib/pages/page_profile.dart
+++ lib/pages/page_profile.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hands_on/pages/page_login.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class PageProfile extends StatelessWidget {
+
+  final _auth = FirebaseAuth.instance;
+//  String email;
+//  String password;
+
+  void getCurrentUser() async {
+    final user = await _auth.currentUser();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
