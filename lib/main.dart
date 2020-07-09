b2760c14f6ac53cc243674b545a359cee1419530
syuheifujita <syuheifujita0117@gmail.com>
2020-07-09 09:13:18 +0900

    PageViewの実装

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_hands_on/components/product_card.dart';
 import 'package:flutter_hands_on/pages/page_product_list.dart';
+import 'package:flutter_hands_on/pages/page_profile.dart';
 import 'package:flutter_hands_on/stores/product_list_store.dart';
+import 'package:flutter_hands_on/stores/tab_bar_store.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_hands_on/pages/product_detail.dart';
 import 'package:flutter_hands_on/stores/product_list_store.dart';
@@ -13,14 +15,10 @@ import 'package:provider/provider.dart';
 void main() async {
   await DotEnv().load('.env.sample');
   runApp(MultiProvider(
-
     providers: [
-
-      ChangeNotifierProvider(
-        create: (context) => ProductListStore(),
-      )
+      ChangeNotifierProvider(create: (context) => ProductListStore()),
+      ChangeNotifierProvider(create: (context) => TabBarStore()),
     ],
-
     child: MyApp(),
   ));
 }
@@ -28,7 +26,6 @@ void main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     final store = Provider.of<ProductListStore>(context, listen: false);
     if(store.products.isEmpty && store.isFetching == false) {
       store.fetchNextProducts();
@@ -51,11 +48,38 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final store = Provider.of<TabBarStore>(context);
+    final controller = PageController();
     return Scaffold(
       appBar: AppBar(
         title: const Text("SUZURI"),
       ),
-      body: PageProductList(),
+      body: PageView(
+        children: <Widget>[
+          PageProductList(),
+          PageProfile(),
+        ],
+        controller: controller,
+        onPageChanged: (index) async {
+          Provider.of<TabBarStore>(context, listen: false)
+              .updateCurrentIndex(index);
+          if (controller.hasClients) {
+            controller.jumpToPage(index);
+          }
+        },
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            title: Text('ホーム'),
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            title: Text('profile'),
+            icon: Icon(Icons.person),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/pages/page_profile.dart
+++ lib/pages/page_profile.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/material.dart';
 
 class PageProfile extends StatelessWidget {
-  static const routeName = "/tabProfile";
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text("profile"),
-        ),
+      body: Center(
+        child: Text("profile"),
       ),
     );
   }
